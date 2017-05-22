47d6e35ccc304d96a9c327fb2c9e0d4b13f8fc70
Azaan Dhalla <amd166@txstate.edu>
2017-05-22 17:55:37 -0500

    Update The Grid Search.py

--- The Grid Search.py
+++ The Grid Search.py
@@ -1,1 +1,23 @@
-Given a 2D array of digits, try to find the occurrence of a given 2D pattern of digits. For example, consider the following 2D matrix:
+#Given a 2D array of digits, try to find the occurrence of a given 2D pattern of digits.
+import sys
+import re
+
+t = int(input().strip())
+for a0 in range(t):
+    temp, expression, G = ['' , '' , '']
+    R,C = input().strip().split(' ')
+    R,C = [int(R),int(C)]
+    for i in range(R):
+        G += str(input().strip()) + '0'
+
+    r,c = input().strip().split(' ')
+    r,c = [int(r),int(c)]
+    for i in range(r):
+        expression += str(input().strip()) 
+        if(i != r - 1):
+             expression += '\d{' + str((C - c + 1)) +'}'
+
+    if(re.sub(expression, "", G) != G):
+        print("YES")
+    else:
+        print("NO")
