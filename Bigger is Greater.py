88e6dab9501326d48597f7550ad521a78255a06f
Azaan Dhalla <amd166@txstate.edu>
2017-05-28 16:16:15 -0500

    Update Bigger is Greater.py

--- Bigger is Greater.py
+++ Bigger is Greater.py
@@ -1,6 +1,6 @@
-Given a word w, rearrange the letters of w to construct another word s in such a way that s is lexicographically greater than w. In case of multiple possible answers, find the lexicographically smallest one among them.
+'''Given a word w, rearrange the letters of w to construct another word s in such a way that s is lexicographically greater 
+than w. In case of multiple possible answers, find the lexicographically smallest one among them.'''
 
-from itertools import permutations
 
 for x in range(int(input().strip())):
     s = input().strip()
